dbc9190e872927819a3d018d1e9ffb4c87b947f0
ddominguez8 <ddominguez8@ucmerced.edu>
2021-01-12 15:50:28 -0800

    player movement along with addition of enemy, movement not done yet

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a tutorial from freeCodeCamp.org that can be followed
 # in the link provided in the readme
 import pygame
-
+import random
 # initialize the pygame
 pygame.init()
 
@@ -13,11 +13,49 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('battleship.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('battleshipPlayer.png')
+playerX = 385
+playerY = 480
+playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 736)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.3
+enemyY_change = 0
+
+def player(x, y):
+    # blit is basically equivalent to draw
+    screen.blit(playerImg, (playerX, playerY))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (enemyX, enemyY))
+
 # Game loop
 running = True
 while running:
+    # Option to change the background of the screen
+    screen.fill((187, 255, 173))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = +0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
-    screen.fill((0,0,0))
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    # Update the display
+    pygame.display.update()
